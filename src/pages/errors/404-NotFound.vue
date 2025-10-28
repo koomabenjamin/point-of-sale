1b0e0d8801408c103456cc0d5a7d48cce74c6de9
Benjamin Kooma <kooma.benjamin@gmail.com>
2025-10-28 10:57:02 +0300

    Add body to 404 page

---
+++ src/pages/errors/404-NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not-found-page">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+
+</style>
